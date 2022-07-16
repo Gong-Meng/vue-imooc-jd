3237047892bafefac2640265494f710e56f1d821
Gong Meng <18588264128@163.com>
2022-07-16 17:37:37 +0800

    开始准备学习

--- meng-hybrid-web/vue.config.js
+++ meng-hybrid-web/vue.config.js
@@ -1,4 +1,23 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require("path")
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      // 为生产环境修改配置...
+    } else {
+      // 为开发环境修改配置...
+      return {
+        resolve: {
+          alias: {
+            '@js': path.resolve(__dirname, './src/assets/js'),
+            '@css': path.resolve(__dirname, './src/assets/css'),
+            '@img': path.resolve(__dirname, './src/assets/imgs'),
+            '@c': path.resolve(__dirname, './src/components')
+          }
+        }
+      }
+    }
+  }
 })
