9a08b01e58879e519d802ee2ac792c435d3170c8
Gong Meng <18588264128@163.com>
2022-07-18 11:51:15 +0800

    活动组件开发

--- meng-hybrid-web/src/components/HomeView.vue
+++ meng-hybrid-web/src/components/HomeView.vue
@@ -1,21 +1,31 @@
 <template>
   <div class="home">
     <div class="home-content">
+      <!-- swiper -->
       <my-swiper :swiper-imgs="swiperData.map(item => item.icon)" :height="swiperHeight"></my-swiper>
+      <!-- activity -->
+      <activity>
+          <div class="activity-520">
+            <img v-for="(item, index) in activityDatas" :key="index" :src="item.icon" alt="">
+          </div>
+      </activity>
     </div>
   </div>
 </template>
 
 <script>
 import MySwiper from '@c/swiper/MySwiperView.vue'
+import Activity from '@c/currency/ActivityView.vue'
 export default {
   components: {
-    MySwiper
+    MySwiper,
+    Activity
   },
   data () {
     return {
       swiperData: [],
-      swiperHeight: '184px'
+      swiperHeight: '184px',
+      activityDatas: []
     }
   },
   created () {
@@ -24,8 +34,16 @@ export default {
   methods: {
     // 获取数据
     initData () {
+      // 获取顶部轮播图片
       this.$http.get('/swiper').then(data => {
         this.swiperData = data.list
+      }).catch(err => {
+        console.log(err)
+      })
+
+      // 获取活动信息
+      this.$http.get('/activitys').then(data => {
+        this.activityDatas = data.list
       }).catch(err => {
         console.log(err)
       })
@@ -35,12 +53,25 @@ export default {
 
 </script>
 <style lang="scss" scoped>
+@import '@css/style.scss';
 .home{
   width: 100%;
   height: 100%;
+  background-color: $bgColor;
 
   &-content {
     height: 100%;
+
+    .activity-520{
+      margin-top: px2rem(-8);
+      border-top-left-radius: px2rem(8);
+      border-top-right-radius: px2rem(8);
+
+      img{
+        display: inline-block;
+        width: 33.33%;
+      }
+    }
   }
 }
 </style>
---
+++ meng-hybrid-web/src/components/currency/ActivityView.vue
@@ -0,0 +1,27 @@
+<template>
+  <!--
+    只是提供了一个空间，用来存放活动的一个内容
+    你的活动是什么样子，我不管。
+  -->
+  <div class="activity">
+      <!-- 定义插槽，展示活动内容 -->
+      <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import '@css/style.scss';
+.activity{
+  max-height: px2rem(160);
+  position: relative;
+  z-index: 2;
+}
+</style>
