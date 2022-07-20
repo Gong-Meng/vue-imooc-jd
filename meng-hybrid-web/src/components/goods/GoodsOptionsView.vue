60650dcf11f8bac4dd2f7d844702cc2a83992c3a
Gong Meng <18588264128@163.com>
2022-07-20 18:23:58 +0800

    筛选器代码逻辑实现

--- meng-hybrid-web/src/components/goods/GoodsOptionsView.vue
+++ meng-hybrid-web/src/components/goods/GoodsOptionsView.vue
@@ -5,25 +5,33 @@
       <li class="goods-options-list-item"
         v-for="(item, index) in optionsDatas" :key="index"
       >
-        <a href="" class="goods-options-list-item-content">
-          <span class="goods-options-list-item-content-name">
+        <a href="javascript:;" class="goods-options-list-item-content"
+          @click="onOptionsItemClick(item, index)"
+        >
+          <span class="goods-options-list-item-content-name"
+            :class="{'goods-options-list-item-content-name-active': selectOption.id === item.id}"
+          >
             {{item.name}}
           </span>
-          <span class="goods-options-list-item-content-caret caret"></span>
+          <span v-if="item.subs.length > 0" class="goods-options-list-item-content-caret caret"></span>
         </a>
       </li>
     </ul>
     <!-- 子选项内容 -->
-    <div class="options-sub-content">
+    <div class="options-sub-content" v-show="isShowSubContent">
       <ul class="options-sub-content-list">
         <li class="options-sub-content-list-item"
           v-for="(item, index) in selectOption.subs" :key="index"
         >
-          <a class="options-sub-content-list-item-content" href="">
-            <span class="options-sub-content-list-item-content-name">
+          <a class="options-sub-content-list-item-content" href="javascript:;"
+            @click="onSubOptionsItemClick(item, index)"
+          >
+            <span class="options-sub-content-list-item-content-name"
+              :class="{'options-sub-content-list-item-content-name-active': selectOption.id === item.id}"
+            >
               {{item.name}}
             </span>
-            <img class="options-sub-content-list-item-content-select" src="@img/options-select.svg" alt="">
+            <img v-show="selectOption.id === item.id" class="options-sub-content-list-item-content-select" src="@img/options-select.svg" alt="">
           </a>
         </li>
       </ul>
@@ -49,7 +57,7 @@ export default {
               name: '价格由高到低'
             },
             {
-              id: '1-2',
+              id: '1-3',
               name: '销量由高到低'
             }
           ]
@@ -66,12 +74,59 @@ export default {
         }
       ],
       // 选中的筛选项
-      selectOption: {}
+      selectOption: {},
+      // 标记子选项是否处于展开状态
+      isShowSubContent: false
     }
   },
   created () {
     this.selectOption = this.optionsDatas[0]
-    console.log(this.selectOption)
+  },
+  methods: {
+    /**
+     * 一级选项点击事件
+     * 1、如果子选项处于展开状态，则关闭子选项试图
+     * 2、筛选项（item）包含子选项（subs.length>0）
+     * 并且当前 item 处于一个选中状态下的时候（item.id === selectOpotion.id），展示子选项视图
+     * 3、设置选中项为用户点击的item
+     */
+    onOptionsItemClick (item, index) {
+      // 1、如果子选项处于展开状态，则关闭子选项试图
+      // 需标记子选项视图是否展开 isShowSubContent
+      if (this.isShowSubContent) {
+        this.isShowSubContent = false
+        return
+      }
+      // 展示子选项视图
+      // 1、（item）包含子选项（subs.length > 0）
+      // 2、当前 item 处于一个选中状态下的时候（item.id === selectOpotion.id）
+      if (item.subs.length > 0 && this.selectOption.id === item.id) {
+        this.isShowSubContent = true
+      }
+      // 3、设置选中项为用户点击的item
+      this.selectOption = item
+    },
+    /**
+     * 子选项点击事件
+     * 1、设置选中项为用户点击的选项（item）
+     * 2、将选中项置顶到一级选项
+     * 3、关闭子选项试图
+     */
+    onSubOptionsItemClick (item, index) {
+      // 设置选中项为用户点击的选项（item）
+      this.selectOption = item
+      // 将选中项置顶到一级选项
+      this.optionsDatas.forEach(options => {
+        options.subs.forEach(subOptions => {
+          if (subOptions.id === this.selectOption.id) {
+            options.id = subOptions.id
+            options.name = subOptions.name
+          }
+        })
+      })
+      // 关闭子选项试图
+      this.isShowSubContent = false
+    }
   }
 }
 </script>
@@ -99,6 +154,10 @@ export default {
         &-name{
           font-size: $infoSize;
           margin-right: $marginSize;
+
+          &-active{
+            color: $mainColor;
+          }
         }
       }
     }
@@ -127,6 +186,10 @@ export default {
             font-size: $infoSize;
             display: inline-block;
             flex-grow: 1;
+
+            &-active{
+              color: $mainColor;
+            }
           }
 
           &-select{
