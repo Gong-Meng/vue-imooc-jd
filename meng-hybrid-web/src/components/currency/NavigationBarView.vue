38107de3bdb5d3c14ab98e29952950910c7f1b21
Gong Meng <18588264128@163.com>
2022-07-20 10:14:55 +0800

    navigationBar 组件设计原则

---
+++ meng-hybrid-web/src/components/currency/NavigationBarView.vue
@@ -0,0 +1,23 @@
+<template>
+  <!--
+    能力：
+    1、默认展示的效果 -> 左边后退按钮的图标、中间页面的名称、右边是空白内容
+    2、可通过插槽来配置具体的展示样式 -> 左，中，右 三个插槽，父组件可以通过插槽进行布局
+    3、可以接收从外部指定的一个样式（可以在父组件中指定 navigationBar 的style）
+   -->
+  <div>
+
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
