8d1adb38f08eb30191ddfdae7cb177c88056043f
Gong Meng <18588264128@163.com>
2022-07-22 10:37:40 +0800

    NavBar 与 MySwiper 的使用

--- meng-hybrid-web/src/views/GoodsDetailView.vue
+++ meng-hybrid-web/src/views/GoodsDetailView.vue
@@ -1,11 +1,47 @@
 <template>
-  <p style="font-size: 32px;">GoodsDetail</p>
+  <div class="goods-detail">
+    <navigation-bar :is-show-back="false" :nav-bar-style="navBarStyle">
+      <template v-slot:nav-left>
+        <div class="goods-detail-nav-left" @click="onBackClick">
+          <img src="@img/back-2.svg" alt="">
+        </div>
+      </template>
+    </navigation-bar>
+    <div class="goods-detail-content">
+      <my-swiper
+        :height="SWIPER_IMAGE_HEIGHT + 'px'"
+        :swiper-imgs="goodsData.swiperImgs"
+      >
+      </my-swiper>
+    </div>
+  </div>
 </template>
 <script>
+import NavigationBar from '@c/currency/NavigationBarView.vue'
+import MySwiper from '@c/swiper/MySwiperView.vue'
 export default {
+  components: {
+    NavigationBar,
+    MySwiper
+  },
   data () {
     return {
-
+      SWIPER_IMAGE_HEIGHT: 364,
+      navBarStyle: {
+        backgroundColor: ''
+      },
+      goodsData: {}
+    }
+  },
+  created () {
+    this.goodsData = this.$route.params.goods
+  },
+  methods: {
+    /**
+     * 后退当前页面
+     */
+    onBackClick () {
+      this.$router.go(-1)
     }
   }
 }
